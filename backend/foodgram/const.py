d2fbb35c7017d5733de1298793594feb3bd9e915
Антон Карпов <antonkarpov@MBP-Anton.Dlink>
2022-03-15 14:57:58 +0300

    v0.2

---
+++ backend/foodgram/const.py
@@ -0,0 +1,5 @@
+EMPTY = '< Пустотааа >'
+ERRORS_KEY = 'errors'
+
+INGREDIENT_MIN_AMOUNT = 1
+COOKING_TIME_MIN_VALUE = 1
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,159 +1,94 @@
-from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db.models import (
     CASCADE,
-    SET_NULL,
+    BooleanField,
     CharField,
+    EmailField,
     ForeignKey,
-    ImageField,
     ManyToManyField,
     Model,
-    PositiveIntegerField,
-    SlugField,
-    TextField,
+    OneToOneField,
     UniqueConstraint
 )
-from django.urls import reverse
 
-from foodgram.constants import COOKING_TIME_MIN_VALUE, INGREDIENT_MIN_AMOUNT
+from .managers import UserManager
 
-User = get_user_model()
-
-COOKING_TIME_MIN_ERROR = (
-    'Время приготовления не может быть меньше одной минуты!'
-)
-INGREDIENT_MIN_AMOUNT_ERROR = (
-    'Количество ингредиентов не может быть меньше {min_value}!'
+SHOPPING_CART_RECIPE_ALREADY_EXISTS_ERROR = (
+    'Данный рецепт уже есть в вашем списке покупок!'
 )
 
 
-class Tag(Model):
-    name = CharField('Название', max_length=200)
-    color = CharField('Цвет в HEX', max_length=7)
-    slug = SlugField('Слаг', max_length=200)
+class User(AbstractBaseUser, PermissionsMixin):
+    email = EmailField('Почта', max_length=254, unique=True)
+    username = CharField('Никнейм', max_length=150)
+    first_name = CharField('Имя', max_length=150)
+    last_name = CharField('Фамилия', max_length=150)
+    password = CharField('Пароль', max_length=150)
+    is_superuser = BooleanField('Администратор', default=False)
+    is_blocked = BooleanField('Заблокирован', default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
 
     class Meta:
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
-
-    def __str__(self):
-        return f'{self.name}'
-
-    def get_absolute_url(self):
-        return reverse('tag', args=[self.slug])
-
-
-class Ingredient(Model):
-    name = CharField('Название', max_length=200)
-    measurement_unit = CharField('Единица измерения', max_length=200)
-
-    class Meta:
-        verbose_name = 'Ингредиент'
-        verbose_name_plural = 'Ингредиенты'
-        ordering = ('name',)
-
-    def __str__(self):
-        return f'{self.name}'
-
-
-class Recipe(Model):
-    name = CharField('Название', max_length=200)
-    text = TextField('Описание')
-    ingredients = ManyToManyField(
-        'CountOfIngredient',
-        related_name='recipes',
-        verbose_name='Ингредиенты'
-    )
-    tags = ManyToManyField(
-        Tag,
-        related_name='recipes',
-        verbose_name='Теги'
-    )
-    image = ImageField('Картинка')
-    cooking_time = PositiveIntegerField(
-        'Время приготовления',
-        validators=(MinValueValidator(
-            COOKING_TIME_MIN_VALUE,
-            message=COOKING_TIME_MIN_ERROR,
-        ),)
-    )
-    author = ForeignKey(
-        User,
-        on_delete=SET_NULL,
-        null=True,
-        related_name='recipes',
-        verbose_name='Автор',
-    )
-
-    class Meta:
-        verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецепты'
         ordering = ('-pk',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        return f'{self.name} ({self.author})'
+        return self.username
 
-    def get_absoulute_url(self):
-        return reverse('recipe', args=[self.pk])
-
-
-class CountOfIngredient(Model):
-    ingredient = ForeignKey(
-        Ingredient,
-        on_delete=CASCADE,
-        related_name='count_in_recipes',
-        verbose_name='Ингредиент',
-    )
-    amount = PositiveIntegerField(
-        'Количество',
-        validators=(MinValueValidator(
-            INGREDIENT_MIN_AMOUNT,
-            message=INGREDIENT_MIN_AMOUNT_ERROR.format(
-                min_value=INGREDIENT_MIN_AMOUNT
-            )
-        ),)
-    )
-
-    class Meta:
-        verbose_name = 'Количество ингредиента'
-        verbose_name_plural = 'Количество ингредиентов'
-        constraints = (
-            UniqueConstraint(
-                fields=('ingredient', 'amount',),
-                name='unique_ingredient_amount',
-            ),
-        )
-
-    def __str__(self):
-        return (
-            f'{self.ingredient.name} - {self.amount}'
-            f' ({self.ingredient.measurement_unit})'
-        )
+    @property
+    def is_staff(self):
+        return self.is_superuser
 
 
-class Favorite(Model):
+class Subscribe(Model):
     user = ForeignKey(
         User,
         on_delete=CASCADE,
-        related_name='favorites',
-        verbose_name='Пользователь',
+        related_name='subscriber',
+        verbose_name='Подписчик'
     )
-    recipe = ForeignKey(
-        Recipe,
+    author = ForeignKey(
+        User,
         on_delete=CASCADE,
-        related_name='favorites',
-        verbose_name='Рецепт',
+        related_name='subscribing',
+        verbose_name='Автор'
     )
 
     class Meta:
-        verbose_name = 'Избранное'
-        verbose_name_plural = 'Избранное'
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
         constraints = (
             UniqueConstraint(
-                fields=('user', 'recipe',),
-                name='unique_user_recipe',
+                fields=('user', 'author',),
+                name='unique_subscribe',
             ),
         )
 
     def __str__(self):
-        return f'{self.user} -> {self.recipe}'
+        return f'{self.user} -> {self.author}'
+
+
+class ShoppingCart(Model):
+    user = OneToOneField(
+        User,
+        on_delete=CASCADE,
+        related_name='shopping_cart',
+        verbose_name='Пользователь',
+    )
+    recipes = ManyToManyField(
+        'recipes.Recipe',
+        related_name='in_shopping_cart',
+        verbose_name='Рецепты',
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+
+    def __str__(self):
+        return f'{self.user}'
