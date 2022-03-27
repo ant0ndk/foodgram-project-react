cee7fbd277f647c9b6ccfa7067dc85e72e30dcb8
Антон Карпов <antonkarpov@MBP-Anton.Dlink>
2022-03-27 11:30:40 +0300

    permission fix

--- backend/foodgram/permissions.py
+++ backend/foodgram/permissions.py
@@ -10,12 +10,9 @@ class IsAuthorOrAdminOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in SAFE_METHODS
-            or (
                 request.user.is_authenticated
                 and (
                     obj.author == request.user
                     or request.user.is_superuser
                 )
             )
-        )
